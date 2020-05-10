6f205668aa629299464548dceff598aca6acdda6
dsying <dingduoduo0916@gmail.com>
2020-05-11 00:31:08 +0800

    公平锁与非公平锁的测试

--- src/main/java/multithread/c_002/T09_FairAndUnFariTest.java
+++ src/main/java/multithread/c_002/T09_FairAndUnFariTest.java
@@ -1,5 +1,7 @@
 package multithread.c_002;
 
+import org.junit.Test;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -14,42 +16,27 @@ public class T09_FairAndUnFariTest {
   private static ReentrantLock unFairLock = new ReentrantLock(false);
 
   public static void main(String[] args) {
-//    testLock(fairLock);
-
-    testLock(unFairLock);
+    testLock(fairLock);
+//    testLock(unFairLock);
   }
+
   private static void testLock(ReentrantLock lock) {
     for(int i = 0; i < 5; i++) {
       new Thread(() -> {
-        try {
-          lock.lock();
-          printLockAndWaiting(lock);
-          TimeUnit.SECONDS.sleep(1);
-        } catch(InterruptedException e) {
-          e.printStackTrace();
-        } finally {
-          lock.unlock();
+        for(int j = 0; j < 2; j++) {
+          try {
+            lock.lock();
+            printLockAndWaiting(lock);
+            TimeUnit.SECONDS.sleep(1);
+          } catch(InterruptedException e) {
+            e.printStackTrace();
+          } finally {
+            lock.unlock();
+          }
         }
-      }, "thread-" + i).start();
+      }, " " + i).start();
     }
   }
-
-
-
-
-  private static void getOwner(ReentrantLock lock) {
-    Method getQueuedThreads = null;
-    Thread result = null;
-    try {
-      getQueuedThreads = lock.getClass().getDeclaredMethod("getOwner");
-      getQueuedThreads.setAccessible(true);
-      result = (Thread) getQueuedThreads.invoke(lock);
-    } catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-      e.printStackTrace();
-    }
-    System.out.print("Lock by:" + result.getName() + ", ");
-  }
-
 
   private static void printLockAndWaiting(ReentrantLock lock) {
     try {
@@ -57,15 +44,13 @@ public class T09_FairAndUnFariTest {
       sync.setAccessible(true);
       AbstractQueuedSynchronizer aqs = (AbstractQueuedSynchronizer) sync.get(lock);
       Collection<Thread> queuedThreads = aqs.getQueuedThreads();
-
-      getOwner(lock);
-
-      System.out.print("Waiting by: [");
+      System.out.print("Lock by:" + Thread.currentThread().getName());
+      System.out.print(", Waiting by: [");
       queuedThreads.stream().sorted(Comparator.comparing(Thread::getName)).forEach(thread -> {
         System.out.print(thread.getName() + ",");
       });
       System.out.print("]");
-      System.out.println("\n");
+      System.out.println("\r");
     } catch(NoSuchFieldException | IllegalAccessException e) {
       e.printStackTrace();
     }
